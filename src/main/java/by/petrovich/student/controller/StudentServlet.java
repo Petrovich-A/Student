a5073f120acd103d047ad317bae07e9e83a4a446
Petrovich-A <A.Piatrovich@gmail.com>
2023-04-13 13:02:54 +0300

    removed trash

--- src/main/java/by/petrovich/student/controller/StudentServlet.java
+++ src/main/java/by/petrovich/student/controller/StudentServlet.java
@@ -16,12 +16,13 @@ import java.util.List;
 
 @WebServlet("/student")
 public class StudentServlet extends HttpServlet {
+    private static final StudentDao STUDENT_DAO = new StudentDaoImpl();
+
     public void init() {
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("do get");
         processRequest(request, response);
     }
 
@@ -31,19 +32,9 @@ public class StudentServlet extends HttpServlet {
     }
 
     private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String str = "str from servlet";
-        request.setAttribute("str", str);
-        StudentDao studentDao = new StudentDaoImpl();
-        List<Student> students = studentDao.receiveAll();
-        System.out.println(students);
+        List<Student> students = STUDENT_DAO.receiveAll();
         request.setAttribute("students", students);
-//        ArrayList<Student> students = new ArrayList<>();
-//        Student student1 = new Student(1, "first name1", "last name1");
-//        Student student2 = new Student(2, "first name2", "last name2");
-//        students.add(student1);
-//        students.add(student2);
-        request.setAttribute("students", students);
-        RequestDispatcher requestDispatcher = request.getRequestDispatcher("main.jsp");
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/pages/main.jsp");
         requestDispatcher.forward(request, response);
     }
 

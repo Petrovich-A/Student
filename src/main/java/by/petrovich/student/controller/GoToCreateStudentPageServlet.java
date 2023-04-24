7918798b3693dd0f2b0a67c003f0a77ffe843143
Alexandr <a.piatrovich@gmail.com>
2023-04-24 18:34:16 +0300

    deleted session

--- src/main/java/by/petrovich/student/controller/GoToCreateStudentPageServlet.java
+++ src/main/java/by/petrovich/student/controller/GoToCreateStudentPageServlet.java
@@ -10,7 +10,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
 
@@ -38,8 +37,6 @@ public class GoToCreateStudentPageServlet extends HttpServlet {
         RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/pages/studentCreate.jsp");
         List<City> cities = CITY_DAO.receiveAll();
         request.setAttribute(CITIES, cities);
-        HttpSession session = request.getSession(true);
-        session.setAttribute(CITIES, cities);
         requestDispatcher.forward(request, response);
     }
 

a142c57f74f361bbf4ebe6395ca05fa358137ddf
Petrovich-A <A.Piatrovich@gmail.com>
2023-05-11 12:50:01 +0300

    removed imports with *

--- src/main/java/by/petrovich/student/model/City.java
+++ src/main/java/by/petrovich/student/model/City.java
@@ -1,12 +1,19 @@
 package by.petrovich.student.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @RequiredArgsConstructor
 @Builder
+@NoArgsConstructor
 public class City {
     private int id;
     @NonNull
